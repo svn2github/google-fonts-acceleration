531f4a1b13bc3bceacbce4182f6534d1092e8407
cqdaidong <cqdaidong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-09-07 05:51:51 +0000

    sjshengji
    git-svn-id: https://plugins.svn.wordpress.org/google-fonts-acceleration@1491541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/mxcdn.php
+++ trunk/mxcdn.php
@@ -1,8 +1,8 @@
 <?php
 /*
-	Plugin Name:谷歌字体与Gravatar头像加速[长期稳定,非360前端库加速源]
+	Plugin Name:谷歌字体与Gravatar头像加速[非360加速源]
 	Plugin URI: http://guowen.party/
-	Description:本插件可针对面对中国大陆地区用户的网站调用的Google前端库服务以及字体库服务和Gravatar头像服务进行替换加速,且支持SSL加密调用加速源，感谢屌鸡提供镜像加速服务器支持。
+	Description:【两年稳定保证,永久免费服务】本插件可免费针对面对中国大陆地区用户的网站调用的Google前端库服务以及字体库服务和Gravatar头像服务进行替换加速,且支持SSL加密调用加速源，感谢屌鸡提供镜像加速服务器支持，本插件的加速源服务器全部为私有，360停止前端公共库服务对我们没有任何影响。
 	Version: 1.8
 	Author: cqdaidong
 	Author URI: http://guowen.party/
